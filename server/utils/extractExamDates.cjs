97f2a2d08988ba956d3a3dc94435d2511c5fa9de
krish-32 <krishnasri9123@gmail.com>
2024-09-07 10:20:14 +0530

    extraction of exam pdf OK

--- server/utils/extractExamDates.cjs
+++ server/utils/extractExamDates.cjs
@@ -1,16 +1,18 @@
-let dates_object={};
-let Forenoon_exam={};
-let afternoon_exam={};
-let i, j;
-
-
+//Function for Exame date PDF's
 async function constructExamDatesFromPDF(pdf) {
+    let dates_object={};
+    //Object for Forenoon session exams with F.N key
+    let Forenoon_exam={};
+    //Object for Afternoon session exams with A.N key
+    let afternoon_exam={};
+    let i, j;
     for (j = 0; j <pdf.Pages.length; j++) {
-        // let dict_data={};
+        //Count variable to getting number of exam from each pdf page
         let count=0;
         for (i = 0; i <= pdf.Pages[j].Texts.length - 1; i++) {
             if(pdf.Pages[j].Texts[i].R[0].T==="Branch%20Name"){
                 i=i+2;
+                //Getting Count
                 while(pdf.Pages[j].Texts[i].R[0].T!="Semes"){
                     count=count+1;
                     i=i+1;
@@ -25,22 +27,22 @@ async function constructExamDatesFromPDF(pdf) {
                 for(let sub=init;sub<=total;sub++){
                     let arr=[];
                     let sorted_array=[];
+                    //Conditional statement for set the values of A.N exams in afternoon object
                     if(pdf.Pages[j].Texts[session].R[0].T==='A.N.'){
                         let date_data=pdf.Pages[j].Texts[date].R[0].T;
-                        let k=0;
                         if(afternoon_exam[date_data]){
-                            afternoon_exam[date_data]=afternoon_exam[date_data]+","+pdf.Pages[j].Texts[sub+k].R[0].T
+                            afternoon_exam[date_data]=afternoon_exam[date_data]+","+pdf.Pages[j].Texts[sub].R[0].T
                             arr=afternoon_exam[date_data].split(",");
                         }else {
-                        //eliminating the dublicates in array
-                        arr.push(pdf.Pages[j].Texts[sub+k].R[0].T)
+                        arr.push(pdf.Pages[j].Texts[sub].R[0].T)
                         }
+                        //eliminating the dublicates in array
                         sorted_array = Array.from(new Set(arr));
                         afternoon_exam[date_data]=sorted_array ;
                         date=date+1;
                         session=session+1;
-                        dates_object['A.N']=afternoon_exam
                     }
+                    //Conditional statement for set the values of F.N exams in forenoon object
                     else{
                         let date_data=pdf.Pages[j].Texts[date].R[0].T;
                         let k=0;
@@ -55,7 +57,6 @@ async function constructExamDatesFromPDF(pdf) {
                         Forenoon_exam[date_data]=sorted_array ;
                         date=date+1;
                         session=session+1;
-                        dates_object['F.N']=Forenoon_exam
                     }
                 }
             }
@@ -67,6 +68,8 @@ async function constructExamDatesFromPDF(pdf) {
         // }
         //time_table.push(TimeTable);       
     }
+    dates_object['F.N']=Forenoon_exam
+    dates_object['A.N']=afternoon_exam
     return dates_object;
     // fs.writeFile(
     //     path.join(__dirname,"output_files","examDates.json"),
